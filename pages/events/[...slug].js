f0be6cb3958dfc5c69a179e21f41b32d7ee7e5a5
Nick Tillinghast <nickt2@cox.net>
2021-03-11 10:27:56 -0700

    added pages and event component with images

---
+++ pages/events/[...slug].js
@@ -0,0 +1,15 @@
+
+
+
+function FilteredEventsPage() {
+    return (
+        <div>
+            <h1>
+                Filtered Events page
+            </h1>
+        </div>
+    )
+}
+
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,11 @@
+function EventDetailPage() {
+    return (
+        <div>
+            <h1>
+                Event Detail Page
+        </h1>
+        </div>
+    )
+
+}
+export default EventDetailPage
