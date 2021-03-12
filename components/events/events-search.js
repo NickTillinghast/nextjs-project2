19c92c0ea8375586b9a7a08076b335ed46df02a6
Nick Tillinghast <nickt2@cox.net>
2021-03-12 10:31:50 -0700

    searchbar added with functionality

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,21 +1,35 @@
+import { useRef } from 'react';
+
 import Button from '../UI/button';
+import classes from './events-search.module.css';
 
 
 
 function EventsSearch(props) {
+    const yearInputRef = useRef();
+    const monthInputRef = useRef();
+
+
+    function submitHandler(event) {
+        event.preventDefault();
+
+        const selectedYear = yearInputRef.current.value;
+        const selectedMonth = monthInputRef.current.value;
+    }
+
     return (
-        <form>
-            <div>
-                <div>
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.controls}>
+                <div className={classes.control}>
                     <label htmlFor='year'>Year</label>
-                    <select id='year'>
+                    <select id='year' ref={yearInputRef}>
                         <option value='2021'>2021</option>
                         <option value='2022'>2022</option>
                     </select>
                 </div>
-                <div>
+                <div className={classes.control}>
                     <label htmlFor='month'>Month</label>
-                    <select id='month'>
+                    <select id='month' ref={monthInputRef}>
                         <option value='1'>January</option>
                         <option value='2'>February</option>
                         <option value='3'>March</option>
@@ -31,7 +45,8 @@ function EventsSearch(props) {
                     </select>
                 </div>
             </div>
-            <Button>Find Events</Button>
+            <Button >Find Events</Button>
         </form>
     )
 }
+export default EventsSearch;
