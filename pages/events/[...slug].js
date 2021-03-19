d8f98d9fedc789df433bfa634bbccc38dbb26efa
Nick Tillinghast <nickt2@cox.net>
2021-03-19 09:03:27 -0600

    finished app with alerts and styling

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,5 +1,12 @@
 import { useRouter } from 'next/router';
 import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import ResultsTitle from '../../components/events/results-title';
+import { Fragment } from 'react';
+import Button from '../../components/UI/button';
+import ErrorAlert from '../../components/UI/error-alert';
+
+
 
 function FilteredEventsPage() {
     const router = useRouter();
@@ -22,20 +29,42 @@ function FilteredEventsPage() {
         numMonth < 1 ||
         numMonth > 12
     ) {
-        return <p>Invalid filter.  Please adjust your values.</p>
+        return (
+            <Fragment>
+                <ErrorAlert>
+                    <p>Invalid filter.  Please adjust your values.</p>
+                </ErrorAlert>
+                <div className='center'>
+                    <Button link='/events'>Show All Events</Button>
+                </div>
+            </Fragment>
+        )
     }
 
     const filteredEvents = getFilteredEvents({
         year: numYear,
         month: numMonth,
     });
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return <Fragment>
+            <ErrorAlert>
+                <p>No events found for the chosen filter.</p>
+            </ErrorAlert>
+            <div className='center'>
+                <Button link='/events'>Show All Events</Button>
+            </div>
+        </Fragment>
+    }
+
+    const date = new Date(numYear, numMonth - 1)
 
     return (
-        <div>
-            <h1>
-                Filtered Events page
-            </h1>
-        </div>
+        <Fragment>
+            <ResultsTitle date={date} />
+
+            <EventList items={filteredEvents} />
+
+        </Fragment>
     )
 }
 
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import { getEventById } from '../../dummy-data';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/UI/error-alert'
 // import EventList from '../../components/events/event-list';
 
 
@@ -15,7 +16,11 @@ function EventDetailPage() {
     const event = getEventById(eventId)
 
     if (!event) {
-        return <p>No event found!</p>
+        return (
+            <ErrorAlert>
+                <p>No event found!</p>
+            </ErrorAlert>
+        )
     }
 
     return (
