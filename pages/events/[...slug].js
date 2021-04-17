19782b85bd6bdb4f928555ed1c047093cef6e3f4
Nick Tillinghast <nickt2@cox.net>
2021-04-16 23:47:54 -0600

    added SSR and getstaticprops

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -32,7 +32,7 @@ function FilteredEventsPage(props) {
     if (!loadedEvents) {
         return <p className='center'>Loading...</p>;
     }
-    const earlyBird = 0;
+    // const earlyBird = 0;
     const filteredYear = filteredData[0];
     const filteredMonth = filteredData[1];
 
